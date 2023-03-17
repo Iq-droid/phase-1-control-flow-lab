cd64b4ab3ec4e1d82c3586927ef35ea4f1f2ac24
Iqra <iqra.ali@student.moringaschool.com>
2023-03-17 05:35:34 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  let result
+  if (someValue <= 400) {
+    return "This one is on me!";
+  }
+  else if (someValue >= 2000 && someValue <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  }
+  else if (someValue > 2500) {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(destinationCity){
+  let result
+  if (destinationCity === NYC) {
+    return 'Ok, sounds good.';
+  }
+  else if (destinationCity === Pittsburgh) {
+    return 'No go.';
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipIsGenerous){
+  let result;
+  switch (tipIsGenerous) {
+    case tipIsGenerous:
+      return "Thank you so much.";
+      break;
+    case theTipIsNotAsGenerous:
+      return "Thank you.";
+      break;
+    default:
+      console.log("Bye.");
+      break;
+  }
 }
